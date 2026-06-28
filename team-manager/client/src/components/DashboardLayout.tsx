da5dfaae6657b296d5d7123655251f5629898109
Godfred Fokuo <godfredfokuo199@gmail.com>
2026-06-28 00:45:10 +0000

    Fix missing trpc import in DashboardLayout

--- team-manager/client/src/components/DashboardLayout.tsx
+++ team-manager/client/src/components/DashboardLayout.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from "@/_core/hooks/useAuth";
+import { trpc } from "@/lib/trpc";
 import { getLoginUrl } from "@/const";
 import { Button } from "./ui/button";
 import { useLocation } from "wouter";
